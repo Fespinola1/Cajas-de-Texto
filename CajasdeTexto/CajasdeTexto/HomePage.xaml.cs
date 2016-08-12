caa6ad2b599e5fbfda88c40d55a2d21f40ade796
Fespinola1 <tecnico_18@Outlook.es>
2016-08-11 22:16:42 -0500

    Cajas de Texto

---
+++ CajasdeTexto/CajasdeTexto/HomePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CajasdeTexto
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+        //Agregamos Nuestro Método
+        private void HDCsave_Clicked(Object sender, EventArgs e)
+        {
+            var nombre = boxNombre.Text;
+            if (!string.IsNullOrEmpty(nombre))
+            {
+                DisplayAlert("Hola Héroes", nombre, "Aceptar");
+            }
+
+        }
+    }
+}
+
